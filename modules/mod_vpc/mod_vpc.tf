a1e4a244f1a212403deeb89ef0095e3129c6068a
udayanms <52168703+udayanms@users.noreply.github.com>
2020-04-05 13:08:13 -0400

    Update mod_vpc.tf

--- modules/mod_vpc/mod_vpc.tf
+++ modules/mod_vpc/mod_vpc.tf
@@ -1,7 +1,8 @@
 locals {
-  params - var.params
+  params = var.params
 }
 
 resource "aws_vpc" "vpc1" {
+  count = local.params.vpc_conf.create_vpc ? 1 :0
   cidr_block = local.params.vpc_conf.cidr_block
 }
